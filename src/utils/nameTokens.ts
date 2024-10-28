78330bd19bc9af934abf8335fd3ec1a56a6b2a8c
junaid-faryad <junaid.faryad@gmail.com>
2024-10-29 02:58:10 +0500

    chore: build update

--- src/utils/nameTokens.ts
+++ src/utils/nameTokens.ts
@@ -1,5 +1,3 @@
-import type { SearchResultRequestResponse } from 'src/types/api';
-
 export const getPartnerTld = (tlds: string | string[]) => {
   const tld = typeof tlds === 'string' || Array.isArray(tlds) ? tlds.toString() : '';
   return tld;
@@ -21,21 +19,4 @@ export function formatWalletAddress(text = '', firstChunk = 4, lastChunk = 4) {
     return `${text.slice(0, firstChunk)}...${text.slice(-lastChunk)}`;
   }
   return text;
-}
-
-export function getFilteredRecommendations(
-  recommendations: SearchResultRequestResponse['pageItems'],
-  searchResults: SearchResultRequestResponse['pageItems'],
-) {
-  // Remove duplicate between search and recommendations
-  const filteredRecommendations = recommendations
-    .filter(
-      (recommendation) =>
-        !searchResults.find(
-          (searchResult) =>
-            searchResult.sld === recommendation.sld && recommendation.tld === searchResult.tld,
-        ),
-    )
-    .filter((item) => item?.status === 'available');
-  return filteredRecommendations;
 }
